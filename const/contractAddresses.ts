2e1bf09a1ce218c6738163b2369589dafdefce01
devzippel <110544211+devzippel@users.noreply.github.com>
2023-04-11 18:21:49 -0300

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -3,7 +3,7 @@ export const NETWORK = BinanceTestnet;
 
 export const nftDropContractAddress = "0x35558Ae78839026aF218ce444965F2bEC9B9c97D"; //NFT
 export const tokenContractAddress = "0x3557E821818A6784be524F71113a869f3f9FCE76"; //Token
-export const stakingContractAddress = "0x00E14c0B7c0DC1f2D2C95E3fc285ead091F34266"; // contrato NFT nova
+export const stakingContractAddress = "0xad11392311bAB12e0fC174C30A6A8A70Fd31BD44"; // contrato NFT nova
 
 export const NFT_COLLECTION_ADDRESS = nftDropContractAddress;
 
