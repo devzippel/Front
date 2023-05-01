20d41bd285eb33c93d43043e23f0d0a7e3c95e54
devzippel <devzippel@gmail.com>
2023-04-30 23:48:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
        <div>
         <Head>
-           <meta property="og:image" content="/thumbnail.jpg" />
+           <meta property="og:image" content="/thumbnail.png" />
         </Head>
       </div>
       <div className={styles.content}>
