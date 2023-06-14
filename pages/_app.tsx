99cc52e97ddf67c0328a6c5f3889408a1158d3ef
devzippel <devzippel@gmail.com>
2023-06-14 15:39:38 -0300

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,50 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, Chain } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
-import  "../const/contractAddresses";
+import "../const/contractAddresses";
 import "../styles/globals.css";
-import '../global-styles/main.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCoffee, faFire } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faCoffee, faFire)
-import Head from 'next/head';
+import "../global-styles/main.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee, faFire } from "@fortawesome/free-solid-svg-icons";
+import Head from "next/head";
+import { useRouter } from 'next/router';
+library.add(fab, faCoffee, faFire);
+
+// Definir um tipo personalizado para os valores válidos de activeChain
+type ValidChain = "binance" | "ethereum";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (    
-    <ThirdwebProvider activeChain="binance">
-      {/* Progress bar when navigating between pages */}
+  const router = useRouter();
+  const { pathname } = router;
+
+  let activeChain: ValidChain = "binance"; // Valor padrão
+
+   // Verificar a página atual e definir a rede correspondente
+   if (pathname === '/mintETH' || pathname === '/nfteth') {
+    activeChain = "ethereum";
+  } else if (pathname === '/mintBNB' || pathname === '/nftbnb') {
+    activeChain = "binance";
+  }
+
+  return (
+    <>
+      <ThirdwebProvider activeChain = {activeChain}>
+        <Navbar />
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+
       <NextNProgress
         color="var(--color-tertiary)"
         startPosition={0.3}
         stopDelayMs={200}
         height={3}
         showOnShallow={true}
-  />
-     <Head>
+      />
+
+      <Head>
         <title>Puppets Coin</title>
         <meta name="description" content="PUPPETS." />
         <meta property="og:title" content="Puppets Coin" />
@@ -30,19 +52,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="PUPPETS." />
         <meta property="og:title" content="Puppets Coin" />
         <meta property="og:description" content="PUPPETS." />
-        <meta property="og:image" content="https://puppetscoin.com/thumbnail.png" />
+        <meta
+          property="og:image"
+          content="https://puppetscoin.com/thumbnail.png"
+        />
         <meta property="og:url" content="https://puppetscoin.com" />
         <meta property="og:type" content="https://puppetscoin.com" />
       </Head>
-      {/* Render the navigation menu above each component */}
-      
-      
-      {/* Render the actual component (page) */}
-      <Component {...pageProps} />
-      <Navbar />
-      </ThirdwebProvider>
+    </>
   );
-
 }
 
 export default MyApp;
