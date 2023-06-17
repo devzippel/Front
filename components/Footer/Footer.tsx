58d0f5e0968c727365cecf205707d7a5d79bfbce
devzippel <devzippel@gmail.com>
2023-06-17 18:35:48 -0300

    Add files via upload

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -14,17 +14,17 @@ export default function Footer() {
 
     const [isCopied, setIsCopied] = useState(false);
     const address = '0xdA2c0CDf7d764F8C587380CAdF7129E5eCb7Efb7';
-  
+
     const handleCopyClick = async () => {
-      try {
-        await navigator.clipboard.writeText(address);
-        setIsCopied(true);
-  
-        // Reset the button state after 2 seconds
-        setTimeout(() => setIsCopied(false), 2000);
-      } catch (err) {
-        console.error('Failed to copy text: ', err);
-      }
+        try {
+            await navigator.clipboard.writeText(address);
+            setIsCopied(true);
+
+            // Reset the button state after 2 seconds
+            setTimeout(() => setIsCopied(false), 2000);
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
+        }
     }
 
     return (
@@ -35,33 +35,33 @@ export default function Footer() {
                 <div>
                     <div>
 
-                        
+
                         <div className={styles.linha}>
                             <a className={styles.linha} href="https://twitter.com/PuppetsCoin" target="_blank">
-                                <FontAwesomeIcon icon={faTwitter}  />
+                                <FontAwesomeIcon icon={faTwitter} className={styles.blueIcon} />
                             </a>
                             <a className={styles.linha} href="https://www.instagram.com/puppetsarts/" target="_blank">
-                                <FontAwesomeIcon icon={faInstagram} size="1x" />                                
+                                <FontAwesomeIcon icon={faInstagram} size="1x" className={styles.instaIcon} />
                             </a>
                             <a className={styles.linha} href="https://discord.gg/vr2q4DUcZN"
                                 target="_blank">
-                                <FontAwesomeIcon icon={faDiscord} size="1x" />                               
+                                <FontAwesomeIcon icon={faDiscord} size="1x" className={styles.disIcon}/>
                             </a>
                             <a className={styles.linha} href="https://t.me/PuppetsCoin" target="_blank">
-                                <FontAwesomeIcon icon={faTelegram} size="1x" />
+                                <FontAwesomeIcon icon={faTelegram} size="1x" className={styles.telIcon} />
                             </a>
                             <a className={styles.linha} href="https://www.facebook.com/PuppetsArtsCoin" target="_blank">
-                                <FontAwesomeIcon icon={faFacebook} size="1x" />
+                                <FontAwesomeIcon icon={faFacebook} size="1x" className={styles.faceIcon} />
                             </a>
                         </div>
 
                         <ul className={styles.listInline}>
-                           <li className={styles.listInline}><a href="https://wiki.puppetscoin.com/" target="_blank">Whitepaper</a></li>                            
+                            <li className={styles.listInline}><a href="https://wiki.puppetscoin.com/" target="_blank">Whitepaper</a></li>
                         </ul>
-                        <div className={styles.quadro}> 0xdA2c0C...9E5eCb7Efb7 
-                        <div onClick={handleCopyClick} style={{ cursor: 'pointer', margin: '4px' }}>
-                             <FontAwesomeIcon icon={faCopy} />
-                        </div>
+                        <div className={styles.quadro}> 0xdA2c0C...9E5eCb7Efb7
+                            <div onClick={handleCopyClick} style={{ cursor: 'pointer', margin: '4px' }}>
+                                <FontAwesomeIcon icon={faCopy} />
+                            </div>
                             {isCopied && <div>Copied!</div>}
                         </div>
 
