398caffb4c6db3be7a95adb3eb9faaad24b96beb
devzippel <devzippel@gmail.com>
2023-06-21 11:17:52 -0300

    Add files via upload

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -1,11 +1,11 @@
 import {Binance } from "@thirdweb-dev/chains";
 export const NETWORK = Binance;
 
-export const nftDropContractAddress = "0x25A878bE056f46Ab4af96e05Fe96517eF1A909FA"; //NFT new BNB
+export const nftDropContractAddress = "0xE2B12125dCa462E8A28b61Cd2dA0D556a2B755a6"; //NFT new BNB
 export const tokenContractAddress = "0xdA2c0CDf7d764F8C587380CAdF7129E5eCb7Efb7"; //Token new
-export const stakingContractAddress = "0x7Ac5302419Ff544cAd26D24d5a449115ACFe059f"; // contrato NFT new
+export const stakingContractAddress = "0x6f907CD8F463AEDaBF05952579De7C058e202D37"; // contrato NFT new
 
-export const NFT_COLLECTION_ADDRESS = "0x25A878bE056f46Ab4af96e05Fe96517eF1A909FA"; //NFT BNB
+export const NFT_COLLECTION_ADDRESS = "0xE2B12125dCa462E8A28b61Cd2dA0D556a2B755a6"; //NFT BNB
 
 
 //export const MARKETPLACE_ADDRESS = "";
